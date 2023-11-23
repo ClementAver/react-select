6c3c770b7647a6d9a508f8d8084d4a284fcb4ea9
ClementAver <clement.aver@yahoo.fr>
2023-11-23 16:34:53 +0100

    [+] Integration test suite 2/? - Adds keyboard support tests - coverage = 98%.

--- src/test/Select.test.tsx
+++ src/test/Select.test.tsx
@@ -5,6 +5,7 @@
 import { beforeEach, expect, it, describe } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { fireEvent } from "@testing-library/react";
 
 import { useState } from "react";
 
@@ -42,17 +43,17 @@ it("Should be rendered.", () => {
 
 describe("Given I am an user.", () => {
   describe("When I click on the custom select.", () => {
-    test("Then the dropdown opens.", async () => {
+    beforeEach(async () => {
       const selectCustomTrigger = document.getElementsByClassName("selectCustom-trigger")[0] as HTMLDivElement;
       await userEvent.click(selectCustomTrigger);
+    });
 
+    test("Then the dropdown opens.", async () => {
       const selectCustom = document.getElementsByClassName("selectCustom")[0] as HTMLDivElement;
       expect(selectCustom.classList.contains("isActive")).toBeTruthy();
     });
 
     test("Then no option is showcased (gray background) or designated ('✓').", async () => {
-      const selectCustomTrigger = document.getElementsByClassName("selectCustom-trigger")[0] as HTMLDivElement;
-      await userEvent.click(selectCustomTrigger);
       const selectCustomOptions = document.getElementsByClassName("selectCustom-option");
 
       for (let option of selectCustomOptions) {
@@ -68,7 +69,16 @@ describe("Given I am an user.", () => {
       test("Then the dropdown is closed.", async () => {
         const selectCustomTrigger = document.getElementsByClassName("selectCustom-trigger")[0] as HTMLDivElement;
         await userEvent.click(selectCustomTrigger);
-        await userEvent.click(selectCustomTrigger);
+
+        const selectCustom = document.getElementsByClassName("selectCustom")[0] as HTMLDivElement;
+        expect(selectCustom.classList.contains("isActive")).not.toBeTruthy();
+      });
+    });
+
+    describe("When I press the 'Escape' key.", () => {
+      test("Then the dropdown is closed.", async () => {
+        // await userEvent.keyboard("{esc}"); -> not working.
+        fireEvent.keyDown(document, { key: "Escape" });
 
         const selectCustom = document.getElementsByClassName("selectCustom")[0] as HTMLDivElement;
         expect(selectCustom.classList.contains("isActive")).not.toBeTruthy();
@@ -110,6 +120,34 @@ describe("Given I am an user.", () => {
         expect(isHover).toBe(1);
         expect(isActive).toBe(1);
       });
+
+      beforeEach(async () => {
+        const selectCustomOptions = document.getElementsByClassName("selectCustom-option");
+        const un = selectCustomOptions[0];
+        await userEvent.click(un);
+        const selectCustomTrigger = document.getElementsByClassName("selectCustom-trigger")[0] as HTMLDivElement;
+        await userEvent.click(selectCustomTrigger);
+      });
+
+      describe("When I press the 'ArrowDown' key to select the next value then press 'Enter'.", async () => {
+        test("The value of the component should be updated to 'Deux'.", async () => {
+          fireEvent.keyDown(document, { key: "ArrowDown" });
+          fireEvent.keyDown(document, { key: "Enter" });
+
+          const selectCustomTrigger = document.getElementsByClassName("selectCustom-trigger")[0] as HTMLDivElement;
+          expect(selectCustomTrigger.textContent).toBe("Deux");
+        });
+      });
+
+      describe("When I press the 'ArrowUp' key to select the previous value then press 'Enter'.", () => {
+        test("The value of the component should be updated to 'Un'.", async () => {
+          fireEvent.keyDown(document, { key: "ArrowUp" });
+          fireEvent.keyDown(document, { key: "Enter" });
+
+          const selectCustomTrigger = document.getElementsByClassName("selectCustom-trigger")[0] as HTMLDivElement;
+          expect(selectCustomTrigger.textContent).toBe("Un");
+        });
+      });
     });
 
     describe("When I select another option.", () => {
@@ -125,21 +163,6 @@ describe("Given I am an user.", () => {
       });
     });
   });
-
-  // describe("When I target the native input using tabulation.", () => {
-  //   describe("When I select the first option using 'ArrowDown' key.", () => {
-  //     test("Then the first option should be assigned as the native input value.", async () => {
-  //       // Focuses on input
-  //       await userEvent.tab();
-
-  //       //!\ The following does not works.
-  //       await userEvent.keyboard("{arrowdown}");
-
-  //       const selectCustomTrigger = document.getElementsByClassName("selectCustom-trigger")[0] as HTMLDivElement;
-  //       expect(selectCustomTrigger.textContent).toBe("Un");
-  //     });
-  //   });
-  // });
 
   describe("When I select the first option of the native select.", () => {
     test("Then this option is assigned to the custom select.", async () => {
