38357d70fe823cabb22ac7d3f798093f5fca9b45
ClementAver <clement.aver@yahoo.fr>
2023-11-24 08:09:26 +0100

    [-] Unused import in test file.

--- src/test/Select.test.tsx
+++ src/test/Select.test.tsx
@@ -2,7 +2,7 @@
  * @vitest-environment jsdom
  */
 
-import { beforeEach, expect, it, describe } from "vitest";
+import { beforeEach, expect, describe } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { fireEvent } from "@testing-library/react";
